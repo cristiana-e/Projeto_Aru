4fee16fca1ea6d39ed196e1abff71bb3506227d5
anakarolinatj <84285859+anakarolinatj@users.noreply.github.com>
2021-05-26 23:01:21 -0300

    fogo andando

--- Assets/Script/Flame.cs
+++ Assets/Script/Flame.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Flame : MonoBehaviour
 {
+    public Rigidbody2D rig;
+    public float velocidade;
+    public float temponadirecao;
+
+    float tempo;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,24 @@ public class Flame : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() 
+    { 
+       
+        tempo+=Time.deltaTime;
+
+       
+        if(tempo>= temponadirecao)
+        {
+            velocidade = -velocidade;
+
+            tempo = 0f;
+        }
+
+        rig.velocity = new Vector2(velocidade, rig.velocity.y);
     }
+
+
+
 }
+
+
